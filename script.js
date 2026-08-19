da2f83eed027ac46ee1c5e82cb713679af088a30
saeid0101 <117608958+saeid0101@users.noreply.github.com>
2026-08-19 21:41:30 +0200

    some improvement

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 /* =========================================================
+   ALWAYS START PAGE AT TOP
+   ========================================================= */
+
+   if ("scrollRestoration" in history) {
+    history.scrollRestoration = "manual";
+  }
+  
+  window.addEventListener("load", () => {
+    window.scrollTo(0, 0);
+  });
+/* =========================================================
    BACKGROUND MUSIC
    ========================================================= */
 
